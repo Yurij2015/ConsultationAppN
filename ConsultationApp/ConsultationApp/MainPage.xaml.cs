6922f951aaeafa577955798d612d801c52efa5fa
mokrijyurij <mokrijyurij@outlook.com>
2020-03-16 17:16:37 +0300

    Add project files.

---
+++ ConsultationApp/ConsultationApp/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConsultationApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Label connectionStateLbl;
+        Label connectionDetailsLbl;
+        public MainPage()
+        {
+            connectionStateLbl = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+            };
+            connectionDetailsLbl = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+            };
+            Content = new StackLayout
+            {
+                Children = { connectionStateLbl, connectionDetailsLbl }
+            };
+            CrossConnectivity.Current.ConnectivityChanged += Current_ConnectivityChanged;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            CheckConnection();
+        }
+
+        // обработка изменения состояния подключения
+        private void Current_ConnectivityChanged(object sender, CollectionChangeEventArgs e)
+        {
+            CheckConnection();
+        }
+
+        // получаем состояние подключения
+        private void CheckConnection()
+        {
+            connectionStateLbl.IsVisible = !CrossConnectivity.Current.IsConnected;
+            connectionDetailsLbl.IsVisible = CrossConnectivity.Current.IsConnected;
+
+            if (CrossConnectivity.Current != null &&
+                CrossConnectivity.Current.ConnectionTypes != null &&
+                CrossConnectivity.Current.IsConnected == true)
+            {
+                var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+                connectionDetailsLbl.Text = connectionType.ToString();
+            }
+        }
+    }
+}
