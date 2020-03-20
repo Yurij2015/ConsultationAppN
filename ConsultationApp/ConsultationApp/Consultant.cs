0068dc07dc43fd73c2802de01a4ce0f75f5fbcde
mokrijyurij <mokrijyurij@outlook.com>
2020-03-20 18:51:24 +0300

    add Consultant class

---
+++ ConsultationApp/ConsultationApp/Consultant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsultationApp
+{
+    class Consultant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Portfolio { get; set; }
+        public string Services { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Consultant consultant = obj as Consultant;
+            return this.Id == consultant.Id;
+        }
+    }
+}
